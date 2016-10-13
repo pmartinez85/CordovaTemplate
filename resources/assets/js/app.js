80afbe955f53da6c18d4bb014efa298807a391fc
Pedro Martinez <pmartinez1085@gmail.com>
2016-10-13 21:34:19 +0200

    underscore

---
+++ resources/assets/js/app.js
@@ -0,0 +1,19 @@
+//Opcio 1 require --> 1 Fitxer en un modul --> En una variable.
+//Opció 2 Import
+
+//var pepita = require('pepita');  // Això instal·laria el paquet pepita amb la comanda "npm install pepita"
+
+//var $ = require('jquery');        ---> Es el mateix que: window.$ = require('jquery');
+
+
+
+
+window.$ = window.jquery = require('jquery');  //Així fem les dos coses a
+windows._ = require('underscore');
+
+var _ = require('underscore');
+
+
+    $(document).ready(function(){
+        $("#msgid").html(_.capitalize("This is Hello World by JQuery"));
+    });
